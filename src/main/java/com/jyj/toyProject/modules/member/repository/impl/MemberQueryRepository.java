90df925c3ec46c5568f58c9b8d791618e078a718
yoonjaej3 <yoonjaej3@gmail.com>
2022-06-09 01:11:47 +0900

    init : MemberQueryRepository 생성

--- src/main/java/com/jyj/toyProject/modules/member/repository/impl/MemberQueryRepository.java
+++ src/main/java/com/jyj/toyProject/modules/member/repository/impl/MemberQueryRepository.java
@@ -1,10 +1,16 @@
 package com.jyj.toyProject.modules.member.repository.impl;
 
 
+import com.jyj.toyProject.modules.member.dto.MemberDto;
+import com.jyj.toyProject.modules.member.dto.QMemberDto;
+import com.jyj.toyProject.modules.member.enums.Type;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.jyj.toyProject.modules.member.entity.QMember.member;
 
 @Repository
 public class MemberQueryRepository  {
@@ -12,5 +18,16 @@ public class MemberQueryRepository  {
 
     public MemberQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<MemberDto>findMemberByType(Type type){
+        return queryFactory.select(new QMemberDto(
+                member.name,
+                member.phone,
+                member.email,
+                member.type
+                ))
+                .from(member)
+                .fetch();
     }
 }
