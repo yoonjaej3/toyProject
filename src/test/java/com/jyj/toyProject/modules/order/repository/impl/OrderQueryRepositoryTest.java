1ccfbb559e8a5c29509e06e829e980330c4d1533
yoonjaej3 <yoonjaej3@gmail.com>
2022-06-14 23:34:39 +0900

    chore : 구매자 주문 정보 조회 테스트 수정

--- src/test/java/com/jyj/toyProject/modules/order/repository/impl/OrderQueryRepositoryTest.java
+++ src/test/java/com/jyj/toyProject/modules/order/repository/impl/OrderQueryRepositoryTest.java
@@ -2,6 +2,8 @@ package com.jyj.toyProject.modules.order.repository.impl;
 
 import com.jyj.toyProject.dummy.MemberDummy;
 import com.jyj.toyProject.dummy.OrderDummy;
+import com.jyj.toyProject.modules.festival.entity.Festival;
+import com.jyj.toyProject.modules.festival.repository.interfaces.FestivalRepository;
 import com.jyj.toyProject.modules.member.entity.Member;
 import com.jyj.toyProject.modules.member.enums.Type;
 import com.jyj.toyProject.modules.member.repository.interfaces.MemberRepository;
@@ -10,12 +12,15 @@ import com.jyj.toyProject.modules.order.entity.Orders;
 import com.jyj.toyProject.modules.order.enums.PayType;
 import com.jyj.toyProject.modules.order.enums.Status;
 import com.jyj.toyProject.modules.order.repository.interfaces.OrderRepository;
+import com.jyj.toyProject.modules.store.entity.Store;
+import com.jyj.toyProject.modules.store.repository.interfaces.StoreRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -33,13 +38,19 @@ class OrderQueryRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
+    @Autowired
+    StoreRepository storeRepository;
+
+    @Autowired
+    FestivalRepository festivalRepository;
+
     @Test
     @DisplayName("구매자 주문 정보 조회")
     void findBuyerByMember() {
 
 
         //given
-        Member member1= Member.builder()
+        Member member= Member.builder()
                 .name("test1")
                 .phone("01000000001")
                 .email("test1@test.com")
@@ -48,23 +59,44 @@ class OrderQueryRepositoryTest {
                 .type(Type.Buyer)
                 .build();
 
-        Orders order1=Orders.builder()
-                .member(member1)
+        memberRepository.save(member);
+
+        Festival festival=Festival.builder()
+                .name("여의도 불꽃놀이 축제")
+                .startDate(LocalDate.of(2022,06,01))
+                .endDate(LocalDate.of(2022,07,01))
+                .build();
+
+        festivalRepository.save(festival);
+
+        Store store=Store.builder()
+                .id(1L)
+                .festival(festival)
+                .name("가게A")
+                .phone("01011112222")
+                .build();
+
+        storeRepository.save(store);
+
+        Orders order=Orders.builder()
+                .member(member)
+                .store(store)
                 .request("요청사항1입니다.")
                 .type(Status.COMPLETE)
                 .payType(PayType.Card)
                 .payDate(LocalDateTime.now())
                 .build();
 
-        Member member= MemberDummy.createMember(1,Type.Buyer,null,null);
-        memberRepository.save(member);
-        Orders order= OrderDummy.createOrder(1,member,Status.COMPLETE,PayType.Card);
         orderRepository.save(order);
 
         //when
-        OrderBuyerDto myOrderBuyer=orderQueryRepository.findBuyerByMember(member).get(0);
+        OrderBuyerDto result=orderQueryRepository.findBuyerByStoreId(1L).get(0);
 
         //then
-        assertEquals(order.getMember(),myOrderBuyer.getMember());
+        assertAll(
+                ()->assertEquals(result.getMember().getType(),Type.Buyer),
+                ()->assertEquals(result.getStore().getId(),1L)
+
+        );
     }
 }
