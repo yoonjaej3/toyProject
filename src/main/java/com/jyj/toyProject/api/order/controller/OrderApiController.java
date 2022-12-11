5ff2c0b1b80ea85d843f0c9f38379048a67a80dd
yoonjaej3 <yoonjaej3@gmail.com>
2022-12-12 02:00:25 +0900

    TODO : orderApiController

---
+++ src/main/java/com/jyj/toyProject/api/order/controller/OrderApiController.java
@@ -0,0 +1,26 @@
+package com.jyj.toyProject.api.order.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    /**
+     *
+     * TODO : 조회
+     * TODO : 1. 회원별 주문 조회
+     * TODO : 2. 주문 전체 조회
+     * TODO : 3. 주문별 상세 조회
+     */
+
+
+
+    //TODO : 등록
+
+    //TODO : 수정
+
+    //TODO : 삭제
+
+}
