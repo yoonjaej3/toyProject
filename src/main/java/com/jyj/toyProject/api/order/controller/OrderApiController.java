d658c3c34d87d9eda39cf665ff17f9f71a23ccdc
yoonjaej3 <yoonjaej3@gmail.com>
2022-12-16 01:46:43 +0900

    TODO : Dto

--- src/main/java/com/jyj/toyProject/api/order/controller/OrderApiController.java
+++ src/main/java/com/jyj/toyProject/api/order/controller/OrderApiController.java
@@ -1,19 +1,22 @@
 package com.jyj.toyProject.api.order.controller;
 
 import com.jyj.toyProject.api.order.dto.OrderDto;
+import com.jyj.toyProject.api.order.entity.Orders;
 import com.jyj.toyProject.api.order.repository.impl.OrderQueryRepository;
+import com.jyj.toyProject.api.order.repository.interfaces.OrderRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
 public class OrderApiController {
 
     private final OrderQueryRepository orderQueryRepository;
+
+    private final OrderRepository orderRepository;
 
     //주문 전체 조회
     @GetMapping("/orders")
@@ -40,7 +43,13 @@ public class OrderApiController {
     }
 
     //TODO : 등록
+    @PostMapping("/orders/")
+    public long register(@RequestBody OrderDto orderDto){
 
+        //orderIndto 만들지 아닐지
+        Optional<Orders> orders = orderRepository.findById(orderDto.getId)
+
+    }
     //TODO : 수정
 
     //TODO : 삭제
