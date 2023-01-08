babae9e65c0983fccc75e662b65d5557aee54da9
yoonjaej3 <yoonjaej3@gmail.com>
2023-01-08 22:53:59 +0900

    test : 커밋 테스트

--- src/main/java/com/jyj/toyProject/ToyProjectApplication.java
+++ src/main/java/com/jyj/toyProject/ToyProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToyProjectApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ToyProjectApplication.class, args);
 	}
