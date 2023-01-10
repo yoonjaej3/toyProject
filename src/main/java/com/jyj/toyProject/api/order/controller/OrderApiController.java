58e6185beae8e93b2455df00be63ad5a3a5b40f1
yoonjaej3 <yoonjaej3@gmail.com>
2023-01-11 02:13:21 +0900

    chore : 주문등록 , 주문취소 controller

--- src/main/java/com/jyj/toyProject/api/order/controller/OrderApiController.java
+++ src/main/java/com/jyj/toyProject/api/order/controller/OrderApiController.java
@@ -2,12 +2,16 @@ package com.jyj.toyProject.api.order.controller;
 
 import com.jyj.toyProject.api.member.repository.impl.MemberQueryRepository;
 import com.jyj.toyProject.api.order.dto.OrderDto;
+import com.jyj.toyProject.api.order.entity.Orders;
 import com.jyj.toyProject.api.order.repository.impl.OrderQueryRepository;
 import com.jyj.toyProject.api.order.repository.interfaces.OrderRepository;
+import com.jyj.toyProject.api.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +21,7 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
-    private final MemberQueryRepository memberQueryRepository;
+    private final OrderService orderService;
 
     //주문 전체 조회
     @GetMapping("/orders")
@@ -45,17 +49,24 @@ public class OrderApiController {
 
     }
 
-//    @PostMapping("/orders/")
-//    public String register(@RequestBody OrderDto orderDto){
-//
-//        Optional<Orders> orders = orderRepository.findById(orderDto.getId());
-//
-//        orderRepository.save(orderDto.toEntiity());
-//
-//        return "등록";
-//    }
-    //TODO : 수정
+    @PostMapping("/orders/register")
+    public String registerOrder(@RequestBody OrderDto orderDto){
 
-    //TODO : 삭제
+        orderRepository.save(orderDto.toEntiity());
+
+        return "등록";
+    }
+
+    @Transactional
+    @PostMapping("/orders/cancel/{id}")
+    public String delete(@PathVariable("id") Long orderId){
+
+        Orders orders = orderRepository.getById(orderId);
+
+        orderService.cancelOrder(orders);
+
+        return "주문취소";
+
+    }
 
 }
