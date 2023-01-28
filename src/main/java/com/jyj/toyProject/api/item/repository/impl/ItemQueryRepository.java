19f9e53d810f216f76adfc4c6bd96ae9dc769dad
yoonjaej3 <yoonjaej3@gmail.com>
2023-01-28 22:12:38 +0900

    chore : ItemQueryRepository find

--- src/main/java/com/jyj/toyProject/api/item/repository/impl/ItemQueryRepository.java
+++ src/main/java/com/jyj/toyProject/api/item/repository/impl/ItemQueryRepository.java
@@ -1,10 +1,21 @@
 package com.jyj.toyProject.api.item.repository.impl;
 
 
+import com.jyj.toyProject.api.item.dto.ItemResponseDto;
+import com.jyj.toyProject.api.item.dto.QItemResponseDto;
+
+
+import static com.jyj.toyProject.api.item.entity.QItem.*;
+
+import com.jyj.toyProject.api.item.entity.Item;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
 
 @Repository
 public class ItemQueryRepository {
@@ -13,4 +24,35 @@ public class ItemQueryRepository {
     public ItemQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
     }
+
+    public List<ItemResponseDto> findStore(String storeName){
+
+        return queryFactory.select(new QItemResponseDto(
+                item.id,
+                item.name,
+                item.price
+        ))
+                .from(item)
+                .where(eqStore(storeName))
+                .fetch();
+
+    }
+
+    private BooleanExpression eqStore(String name) {
+
+        if (StringUtils.hasText(name))
+            return item.store.name.eq(name);
+
+        return null;
+    }
+
+    public Item findItemByItdmId(String itemId){
+
+        return queryFactory.select(item)
+                .from(item)
+                .where(item.id.eq(itemId))
+                .fetch().get(0);
+
+    }
+
 }
